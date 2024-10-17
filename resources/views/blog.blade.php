40ab3c5f0e0c475504dda2f8c48bcb9debbe2085
unknown <samuelberkathulu@gmail.com>
2024-10-18 00:17:19 +0700

    init : Blade Templating Engine add : Tailwindcss

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -113,13 +113,13 @@
 
   <header class="bg-white shadow">
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home Page</h1>
+      <h1 class="text-3xl font-bold tracking-tight text-gray-900">Blog Page</h1>
     </div>
   </header>
 
   <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      <p>Welcome to my home page</p>
+      <p>Welcome to blog page</p>
     </div>
   </main>
 
--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', ['pemilik' => 'Samuel Berkat Hulu']);
 });
 Route::get('/about', function () {
     return view('about', ['nama' => 'Samuel Berkat Hulu']);
 });
-Route::get('/home', function () {
-    return view('home', ['pemilik' => 'Samuel Berkat Hulu']);
+Route::get('/blog', function () {
+    return view('blog');
+});
+Route::get('/contact', function () {
+    return view('contact');
 });
